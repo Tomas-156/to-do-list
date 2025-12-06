09b4283627a29c85b9d7056e82bfbaf3bd988a63
Tomas-156 <tomasurban665@gmail.com>
2025-12-06 12:32:58 +0100

    Set up basic MVVM project structure

---
+++ ToDoList/Model/TaskModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Model
+{
+    class TaskModel
+    {
+        private string _description;
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        private bool _isDone;
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+
+        public TaskModel(string description, bool isDone)
+        {
+            this.Description = description;
+            this.IsDone = isDone;
+        }
+    }
+}
