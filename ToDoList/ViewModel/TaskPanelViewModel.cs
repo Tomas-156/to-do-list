5f2c1a45f3cf18e976d835a583815d1f18111960
Tomas-156 <tomasurban665@gmail.com>
2025-12-21 19:55:08 +0100

    Add UI resources and TaskPanelView

--- ToDoList/ViewModel/TaskPanelViewModel.cs
+++ ToDoList/ViewModel/TaskPanelViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ToDoList.ViewModel
 {
-    class TaskPanelViewModel
+    class TaskPanelViewModel : ViewModelBase
     {
     }
 }
--- ToDoList/ViewModel/TaskViewModelcs.cs
+++ ToDoList/ViewModel/TaskViewModelcs.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ToDoList.ViewModel
 {
-    class TaskViewModelcs
+    class TaskViewModelcs : ViewModelBase
     {
     }
 }
